8dca3cdbace64d1b2acee87d549c3a0cf78e04a7
Shwads <adam.rasool99@gmail.com>
2024-07-05 15:27:27 +0900

    Add some basic command line argument handling.

--- main.go
+++ main.go
@@ -1,10 +1,75 @@
 package main
 
 import (
+	"Creatine/pkg/job"
+	"flag"
 	"fmt"
+	"net/url"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello world!")
-    //job := job.Job{}
+	verbose := flag.Bool("v", false, "print all response content")
+	printToFile := flag.Bool("pf", false, "write the output to a file\n")
+	printToConsole := flag.Bool("pc", true, "Print the response to the console. Default true.")
+
+	var method *string
+
+	method = flag.String("m", "GET", "Used to specify the http request method.\n")
+
+	flag.Parse()
+
+	*method = strings.ToUpper(*method)
+
+	switch *method {
+	case "GET":
+		fmt.Println("GET method requested.")
+        fmt.Println("There's nothing here yet m8, get to it")
+		break
+	case "POST":
+		fmt.Println("POST method requested.")
+        fmt.Println("There's nothing here yet m8, get to it")
+		break
+	case "PATCH":
+		fmt.Println("PATCH method requested.")
+        fmt.Println("There's nothing here yet m8, get to it")
+		break
+	case "PUT":
+		fmt.Println("PUT method requested.")
+        fmt.Println("There's nothing here yet m8, get to it")
+		break
+	case "DELETE":
+		fmt.Println("DELETE method requested.")
+        fmt.Println("There's nothing here yet m8, get to it")
+		break
+	default:
+		fmt.Println("Please use one of the following supported methods:")
+		fmt.Print("GET\nPOST\nPATCH\nPUT\nDELETE\n")
+		return
+	}
+
+	suppliedArgs := flag.Args()
+
+	if len(suppliedArgs) < 1 {
+		fmt.Println("Please prove a URL to which to send the request.")
+		return
+	}
+
+	url, urlParseErr := url.Parse(suppliedArgs[0])
+	if urlParseErr != nil {
+		fmt.Printf("Encountered error: %s\n", urlParseErr)
+		fmt.Println("Please provide a valid URL")
+		return
+	}
+
+	job := job.Job{
+		Url:            url,
+		Verbose:        *verbose,
+		PrintToFile:    *printToFile,
+		PrintToConsole: *printToConsole,
+		Method:         *method,
+	}
+
+	job.ProcessJob()
+
 }
